4fcfc3253fe9be4838754286b6689b9beef4df15
aman <aman.ajmaani@gmail.com>
2021-10-15 22:44:42 +0530

    [App] Create base Layour for the app

---
+++ src/navigation/Navigation.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+/* NAVIGATION */
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+/* COMPONENTS */
+import Search from '../screens/Search';
+import Details from '../screens/Details';
+
+const Navigation = () => {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Search">
+        <Stack.Screen name="Search" component={Search} />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
