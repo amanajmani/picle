a027a52ef51237ecaacec4c6b03608076a21f2c6
aman <aman.ajmaani@gmail.com>
2021-10-16 19:54:57 +0530

    [Network] Optimize and handle error cases

--- src/helpers/callApi.helpers.js
+++ src/helpers/callApi.helpers.js
@@ -2,11 +2,20 @@ function handleError(error) {
   return Promise.reject(error);
 }
 
+const STATUS_OK = 200;
+const NOT_FOUND = 400;
+
 export default async function callApi(url, config) {
   try {
     const res = await fetch(url, config);
-    const data = await res.json();
-    return data;
+    if (res.status === STATUS_OK) {
+      const data = await res.json();
+      return data;
+    }
+    if (res.status === NOT_FOUND) {
+      return {endOfList: true, hits: [], totalHits: 0};
+    }
+    throw new Error('Response status not OK');
   } catch (err) {
     return handleError(err);
   }
