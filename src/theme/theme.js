87ca8a0ba90580686ecc40d94051a8a61e9fea03
aman <aman.ajmaani@gmail.com>
2021-10-17 10:19:00 +0530

    [Theme] Add global theme

---
+++ src/theme/theme.js
@@ -0,0 +1,42 @@
+/* UTILS */
+import scale from '../utils/scale.utils';
+
+const theme = {
+  palette: {
+    primary: {
+      main: '#1F2326',
+      dark: '#1B1F22',
+    },
+    secondary: {
+      main: '#80BB65',
+    },
+    text: {
+      primary: '#fff',
+      secondary: '#C0C0C0',
+    },
+  },
+  shape: {
+    borderRadius: 8,
+  },
+  typography: {
+    h1: {
+      fontWeight: 'normal',
+      fontSize: scale(20),
+    },
+    h6: {
+      fontWeight: 'bold',
+      fontSize: scale(18),
+    },
+    body1: {
+      fontWeight: 'bold',
+      fontSize: scale(16),
+    },
+    body2: {
+      fontWeight: 'normal',
+      fontSize: scale(16),
+    },
+  },
+  spacing: factor => 2 * factor,
+};
+
+export default theme;
