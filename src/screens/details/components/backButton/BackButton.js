ce85fba54fc296e1d072360e069ab1c2f1c9604e
aman <aman.ajmaani@gmail.com>
2021-10-17 10:23:43 +0530

    [BackButton] Update styles

--- src/screens/details/components/backButton/BackButton.js
+++ src/screens/details/components/backButton/BackButton.js
@@ -2,9 +2,15 @@ import React from 'react';
 import {TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
+import theme from '../../../../theme/theme';
+
 const BackButton = ({navigation}) => (
   <TouchableOpacity onPress={() => navigation.goBack()}>
-    <Icon name="chevron-back-outline" size={30} color="#fff" />
+    <Icon
+      name="chevron-back-outline"
+      size={30}
+      color={theme.palette.text.primary}
+    />
   </TouchableOpacity>
 );
 
