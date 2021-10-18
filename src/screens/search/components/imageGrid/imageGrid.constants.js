3b124d46af0e1ea2d715a017c6b99309446d1b9b
aman <aman.ajmaani@gmail.com>
2021-10-18 14:51:23 +0530

    [ImageGrid] Handle no results case

---
+++ src/screens/search/components/imageGrid/imageGrid.constants.js
@@ -0,0 +1,3 @@
+export const ELEVATION = 10;
+export const NO_RESULTS_PRIMARY = 'Oops! No results found.';
+export const NO_RESULTS_SECONDARY = `Please check spelling or try ${'\n'} different keywords`;
--- src/screens/search/constants/searchInfoText.constants.js
+++ src/screens/search/constants/searchInfoText.constants.js
@@ -1,3 +1,2 @@
 export const DEFAULT_TEXT = 'Enter the world of picle and type away';
-export const NO_RESULTS = 'Sorry, no picle results found';
 export const PLACEHOLDER = 'Search for amazing content';
