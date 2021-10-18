ceda80e0f6254bfa76d4c442a3203ec384f87c78
aman <aman.ajmaani@gmail.com>
2021-10-18 13:59:48 +0530

    [Services] Update per page network calls

--- src/services/search.services.js
+++ src/services/search.services.js
@@ -5,6 +5,6 @@ import {BASE_URL} from '../constants/baseURL.constants';
 import callApi from '../helpers/callApi.helpers';
 
 export function fetchImages(query, offset) {
-  const url = `${BASE_URL}23859345-3ef4a0e071920cfc7b6e3efc7&q=${query}&image_type=photo&pretty=true&per_page=200&page=${offset}`;
+  const url = `${BASE_URL}23859345-3ef4a0e071920cfc7b6e3efc7&q=${query}&image_type=photo&pretty=true&per_page=50&page=${offset}`;
   return callApi(url);
 }
