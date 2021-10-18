b253d7308e1bb81e8a5bf957746a28b4b450bcff
aman <aman.ajmaani@gmail.com>
2021-10-19 01:22:38 +0530

    [Services] Update Base URL

--- src/services/search.services.js
+++ src/services/search.services.js
@@ -1,10 +1,12 @@
 /* CONSTANTS */
-import {BASE_URL} from '../constants/baseURL.constants';
+import BASE_URL from '../constants/baseURL.constants';
 
 /* HELPERS */
 import callApi from '../helpers/callApi.helpers';
 
 export function fetchImages(query, offset) {
-  const url = `${BASE_URL}23859345-3ef4a0e071920cfc7b6e3efc7&q=${query}&image_type=photo&pretty=true&per_page=50&page=${offset}`;
+  const url = `${BASE_URL}=${encodeURIComponent(
+    query,
+  )}&image_type=photo&per_page=50&page=${offset}`;
   return callApi(url);
 }
