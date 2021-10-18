1c741e28fc094bb0eeb24826e85a849dc4330f27
aman <aman.ajmaani@gmail.com>
2021-10-18 14:03:59 +0530

    [Details] Update styles

--- src/screens/details/details.styles.js
+++ src/screens/details/details.styles.js
@@ -1,8 +1,20 @@
 import {StyleSheet} from 'react-native';
 
+import theme from '../../theme/theme';
+
 const styles = StyleSheet.create({
   flex: {
     flex: 1,
+  },
+  root: {
+    padding: theme.spacing(8),
+  },
+  portrait: {
+    flexDirection: 'column',
+  },
+  landscape: {
+    flexDirection: 'row-reverse',
+    alignItems: 'center',
   },
 });
 
